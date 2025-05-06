1ba01e81118a314e0f435148f9b7fd3722bdb3ee
michel <michel.rodriguez75@gmail.com>
2025-05-06 13:12:21 +0200

    missing

---
+++ scripts/scores_display.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+
+@onready var out_label: Label = get_node("HostageScore/MarginContainer/GridContainer/OutContainer/OutCount")
+@onready var saved_label: Label = get_node("HostageScore/MarginContainer/GridContainer/SavedContainer/SaveCount")
+@onready var dead_label: Label = get_node("HostageScore/MarginContainer/GridContainer/DeatContainer/DeadCount")
+@onready var detained_label: Label = get_node("HostageScore/MarginContainer/GridContainer/DetainedContainer/DetainedCount")
+@onready var heli_label : Label = get_node("HostageScore/MarginContainer/GridContainer/HeliContainer/HeliCount")
+@onready var heli_lives_label : Label = get_node("HeliRemaining/HBoxContainer/Helilives")
+
+func _ready() -> void:
+	Messenger.update_scores.connect(self.update_scores)
+
+func update_scores() -> void:
+	saved_label.text = str(GameVariables.saved_count)
+	detained_label.text = str(GameVariables.detained_count)
+	dead_label.text = str(GameVariables.dead_count)
+	out_label.text = str(GameVariables.out_count)
+	heli_label.text = str(GameVariables.heli_count)
+	heli_lives_label.text = str(GameVariables.heli_lives)
+	
